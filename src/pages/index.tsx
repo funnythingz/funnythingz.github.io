bc7b937e17b7fcfff704de74d9394805fd5e0dd1
funnythingz <funnythingz69+github@gmail.com>
2021-04-16 00:47:39 +0900

    rendered images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Zx1SVG from "../images/zx_1.svg"
+import Zx2SVG from "../images/zx_2.svg"
+import Zx3SVG from "../images/zx_3.svg"
+import Zx4SVG from "../images/zx_4.svg"
+import Zx5SVG from "../images/zx_5.svg"
+import Zx6SVG from "../images/zx_6.svg"
 
 const IndexPage = () => (
   <Layout>
@@ -11,7 +17,7 @@ const IndexPage = () => (
     <p>Welcome to Making Machinez.</p>
     <p>This site is so funny diary site.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <img src="/svg/zx.svg" />
+      <img src={ Zx1SVG } alt="" />
     </div>
     <ul>
       <li><Link to="/archives">原付改造ブログ</Link></li>
