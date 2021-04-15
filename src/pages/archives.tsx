705dca89da89a60800524f1448f4425ad64ffd3d
funnythingz <funnythingz69+github@gmail.com>
2021-04-15 23:47:41 +0900

    Fixed tsx

--- src/pages/archives.tsx
+++ src/pages/archives.tsx
@@ -8,6 +8,7 @@ const Archives: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <SEO title="原付改造ブログ" />
     <h1>原付改造ブログ</h1>
+    <p>ライブディオ/ライブディオZX/スーパーディオを改造カスタムする日々の記録</p>
   </Layout>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="原付改造" />
     <h1>Hi gentsuki kids!</h1>
     <p>Welcome to Making Machinez.</p>
     <p>This site is so funny diary site.</p>
@@ -14,9 +14,9 @@ const IndexPage = () => (
       <img src="/svg/zx.svg" />
     </div>
     <ul>
-      <li><Link to="/archives/">原付改造ブログ</Link></li>
-      <li><Link to="/about/">About</Link></li>
-      <li><Link to="/using-typescript/">Go to "Using TypeScript"</Link></li>
+      <li><Link to="/archives">原付改造ブログ</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/using-typescript">Go to "Using TypeScript"</Link></li>
     </ul>
   </Layout>
 )
