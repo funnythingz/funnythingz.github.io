428bd78e8c8d70095c1d38efe2d882851950c7e4
funnythingz <funnythingz69+github@gmail.com>
2021-05-07 01:37:04 +0900

    added archives2

---
+++ src/pages/archives2.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Visual from "./imgs/archives_image.jpg"
+
+const Archives: React.FC<PageProps<DataProps>> = ({ data, path }) => (
+  <Layout>
+    <SEO title="原付改造ブログ" />
+    <h1>原付改造ブログ</h1>
+    <img src={Visual} />
+    <p>ライブディオ/ライブディオZX/スーパーディオを改造カスタムする日々の記録</p>
+    {data.allMarkdownRemark.group.map(({ tag, totalCount, edges }) => (
+      <article key={tag}>
+        <h1>{tag} <sub>{totalCount}</sub></h1>
+        {edges.map(({ node }) => (
+          <section key={node.frontmatter.slug}>
+            {node.frontmatter.cover &&
+              <Image fixed={node.frontmatter.cover.childImageSharp.fixed} />
+            }
+            <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+            <h2>
+              <Link to={node.frontmatter.slug}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+          </section>
+        ))}
+      </article>
+    ))}
+  </Layout>
+)
+
+export default Archives
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+        edges {
+          node {
+            frontmatter {
+              title
+              slug
+              date(formatString: "YYYY年M月-D日")
+              cover {
+                childImageSharp {
+                  fixed(width: 320, height: 240) {
+                    ...GatsbyImageSharpFixed
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
