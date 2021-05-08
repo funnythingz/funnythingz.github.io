a3887d48edc31c3cf80bfcee22ab7643a8bd762e
funnythingz <funnythingz69+github@gmail.com>
2021-05-08 23:01:12 +0900

    Fixed index layout

--- src/pages/archives.tsx
+++ src/pages/archives.tsx
@@ -15,44 +15,52 @@ const Archives: React.FC<PageProps<DataProps>> = ({ data, path }) => (
     {data.allMarkdownRemark.group.map(({ tag, edges }) => (
       <article key={tag}>
         <h1 style={{
-          fontSize: `1.3rem`
+          fontSize: `1.3rem`,
+          margin: `1.5rem 0 1rem`
         }}>{tag}</h1>
-        {edges.map(({ node }) => (
-          <section key={node.frontmatter.slug}>
-            <div style={{
-              maxWidth: `320px`,
-            }}>
-              <Link to={node.frontmatter.slug} style={{
-                display: `inline-block`,
-                textDecoration: `none`,
-                margin: `0 0 1rem`
+        <section style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `flex-start`
+        }}>
+          {edges.map(({ node }) => (
+            <article key={node.frontmatter.slug}>
+              <div style={{
+                maxWidth: `210px`,
+                margin: `0 0.5rem 0.25rem`
               }}>
-                <figure style={{
-                  margin: `0 0 0.5rem`
+                <Link to={node.frontmatter.slug} style={{
+                  display: `inline-block`,
+                  textDecoration: `none`,
+                  margin: `0 0 1rem`
                 }}>
-                  {node.frontmatter.cover &&
-                    <Image fixed={node.frontmatter.cover.childImageSharp.fixed} style={{
-                      borderRadius: `8px`,
-                      verticalAlign: `bottom`
-                    }} />
-                  }
-                </figure>
-                <h2 style={{
-                  color: `#000`,
-                  fontSize: `1rem`,
-                  lineHeight: `1.1rem`,
-                  margin: 0,
-                }}>
-                  {node.frontmatter.title}
-                </h2>
-                <time dateTime={node.frontmatter.date} style={{
-                  fontSize: `0.7rem`,
-                  color: `#666`,
-                }}>{node.frontmatter.date}</time>
-              </Link>
-            </div>
-          </section>
-        ))}
+                  <figure style={{
+                    margin: `0 0 0.5rem`
+                  }}>
+                    {node.frontmatter.cover &&
+                      <Image fixed={node.frontmatter.cover.childImageSharp.fixed} style={{
+                        borderRadius: `8px`,
+                        verticalAlign: `bottom`
+                      }} />
+                    }
+                  </figure>
+                  <h2 style={{
+                    color: `#000`,
+                    fontSize: `0.9rem`,
+                    lineHeight: `1.2rem`,
+                    margin: 0,
+                  }}>
+                    {node.frontmatter.title}
+                  </h2>
+                  <time dateTime={node.frontmatter.date} style={{
+                    fontSize: `0.7rem`,
+                    color: `#666`,
+                  }}>{node.frontmatter.date}</time>
+                </Link>
+              </div>
+            </article>
+          ))}
+        </section>
       </article>
     ))}
   </Layout>
@@ -73,7 +81,7 @@ export const query = graphql`
               date(formatString: "YYYY年M月D日")
               cover {
                 childImageSharp {
-                  fixed(width: 320, height: 200) {
+                  fixed(width: 210, height: 120) {
                     ...GatsbyImageSharpFixed
                   }
                 }
