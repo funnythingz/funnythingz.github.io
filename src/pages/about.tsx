116f1c057cff6ca5f71b67060549c0512af5fc41
funnythingz <funnythingz69+github@gmail.com>
2021-05-25 22:16:15 +0900

    Fixed about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,39 +7,9 @@ import SEO from "../components/seo"
 const About: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <SEO title="About" />
-    <h1 className="pageTitle">Hello! Making Machinez</h1>
+    <h1 className="pageTitle">Hello! funnythingz world!</h1>
     <p>
-      このサイトは、2004年～2006年まで "makingmachine.com" で公開されていた原付改造日記を再構築したものです。
-      <br />
-      (※ "makingmachine.com" は2006年に閉鎖。)
-      <br />
-      再構築の際にはMovableType4.2(以下MT)を使って旧サイトでは手書きで更新していた日記をブログにしました。
-      <br />
-      デザインやコンテンツは旧サイト当時のものをそのまま流用し、Web標準に基づいたXHTML+CSSによってゼロからソースコードをかきなおしました。
-    </p>
-    <p>
-      そして、2009年1月、"makingmachinez.com" と語尾に "z" をつけて完全復活しました。
-      <br />
-      2010年9月、MovableTypeのバージョンを5にアップグレードし、テンプレートも一新しました。(デザイン変更なし)
-      <br />
-      ここで紹介されてる原付たちは4台中4台すべてが廃車になっています。
-      <br />
-      とまぁだらだら書き綴りましたが、このサイトが原付小僧たちにとって原付を改造する際に役に立てれば良いかなと思ってます。
-    </p>
-    <p>
-      ↑のリニューアルからさらに5年が経ったのでデザインリニューアルしました。どちらかというとPCよりもスマートフォン寄りのデザインに変更しました。と言ってもそんなに変わってないですが…。
-    </p>
-    <p>
-      さらに2015年8月、middlemanに載せ替えました！GitHub Pagesとして公開しています。
-    </p>
-    <p>
-      そして2017年4月末、Bootstrap4でDOMをすべてリファクタリングしました。
-    </p>
-    <p>
-      そして2021年5月、ReactベースのGatsbyでUIからすべてを完全に作り変えています。
-    </p>
-    <p>
-      何はともあれ、今後ともMaking Machinezをよろしくお願いいたします。
+      This site making for funny people! because I'm funnythingz!
     </p>
   </Layout>
 )
