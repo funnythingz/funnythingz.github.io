3d56b12385ce01df5d85e36d804332f374131f49
funnythingz <funnythingz69+github@gmail.com>
2021-06-29 23:53:27 +0900

    Updates

---
+++ component---src-pages-404-tsx-d47e7f02c2bb2c0ca73e.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{9786:function(e,t,a){"use strict";a.r(t);var n=a(7294),s=a(3514),u=a(8972);t.default=function(){return n.createElement(s.Z,null,n.createElement(u.Z,{title:"404: Not found"}),n.createElement("h1",{className:"pageTitle"},"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-d47e7f02c2bb2c0ca73e.js.map
